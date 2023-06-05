52b3cfc05c8a9e0ba59832acf673c0b2eb5cb014
Courage <couragesblog@gmail.com>
2023-06-05 14:45:09 +0100

    Test students class

--- tests/students.spec.ts
+++ tests/students.spec.ts
@@ -2,7 +2,7 @@ import Students from "../src/students";
 import User from "../src/user";
 
 describe('Students', () => {
-    it('it registers a user as student', () => {
+    it('registers a user as student', () => {
         const user = new User({
             firstName: "Courage",
             lastName: "Francis"
@@ -12,6 +12,48 @@ describe('Students', () => {
         students.addUser(user)
 
         expect(students.all.length).toBe(1);
-
     })
+
+    it('registers more than a user as student', () => {
+        const user1 = new User({
+            firstName: "Courage",
+            lastName: "Francis"
+        });
+
+        const user2 = new User({
+            firstName: "Foo",
+            lastName: "Bar"
+        });
+
+        const students = new Students;
+        students.addUser(user1)
+        students.addUser(user2)
+
+        expect(students.all.length).toBe(2);
+    })
+
+    // it('fails when wrong data type is inserted', () => {
+       
+    //     const students = new Students;
+        
+
+    //     expect(students.addUser(new User({
+    //         firstName: '',
+    //         lastName: ''
+    //     }))).toThrow(Error);
+    // })
+
+    it('is an instance of user', () => {
+        const user = new User({
+            firstName: "Courage",
+            lastName: "Francis"
+        })
+
+        const students = new Students;
+        students.addUser(user)
+
+        expect(students.getUser(0)).toBeInstanceOf(User)
+    })
+
+
 })
